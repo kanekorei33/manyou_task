fa40db91be493de1ca10c88da14bb744ac9f48b9
kaneko <kanekorei33@gmail.com>
2022-07-11 11:53:23 +0900

    feat:タームゾーンを日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module ManyouTask
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.generators do |g|
