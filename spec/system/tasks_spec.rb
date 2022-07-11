50779efb5e36df3d441e990dba8fb418628bb934
kaneko <kanekorei33@gmail.com>
2022-07-11 14:12:59 +0900

    feat:タスク一覧を作成日時の順番で並び替えられているかのテストを実装

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
+
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task) { FactoryBot.create(:task, title: 'task') }
+  before do
+    visit tasks_path
+  end
+
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
         fill_in "task[title]", with: "タスク表示"
         fill_in "task[content]", with: "テストします"
-        click_on "Create Task"
+        click_on "登録"
         expect(page).to have_content 'タスク表示'
       end
     end
@@ -14,9 +20,13 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, title: 'task')
-        visit tasks_path
         expect(page).to have_content 'task'
+      end
+    end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task_list = all('.task_row')[0]
+        expect(page.text).to match 'task'
       end
     end
   end
