76d65886ca9d63558639a9d74b6c6f0e52d694b4
kaneko <kanekorei33@gmail.com>
2022-07-13 06:40:50 +0900

    ディフォルト値の追加をするため終了期限のカラムの作り直しとステータス表示の為のカラム追加

--- db/migrate/20220712051637_add_deadline_to_tasks.rb
+++ db/migrate/20220712051637_add_deadline_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddDeadlineToTasks < ActiveRecord::Migration[6.0]
   def change
-    add_column :tasks, :deadline, :datetime, null: false
+    add_column :tasks, :deadline, :datetime, default: -> { 'NOW()' }, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_12_051637) do
+ActiveRecord::Schema.define(version: 2022_07_12_090815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,8 @@ ActiveRecord::Schema.define(version: 2022_07_12_051637) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deadline", null: false
+    t.datetime "deadline", default: -> { "now()" }, null: false
+    t.integer "status"
   end
 
 end
