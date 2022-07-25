da0872582ca79a92ecf2d09b92f68061d6c81656
kaneko <kanekorei33@gmail.com>
2022-07-26 03:18:25 +0900

    feat:管理画面のmodelのコールバック実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -18,7 +18,7 @@ before_action :set_user, only: %i[ show edit update destroy ]
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to admin_user_path(@user), notice: "アドミンユーザーを登録しました"
+      redirect_to admin_user_path(@user), notice: "ユーザー#{@user.name}を登録しました"
     else
       render :new
     end
@@ -37,15 +37,20 @@ before_action :set_user, only: %i[ show edit update destroy ]
   def update
  #   binding.pry
     if @user.update(user_params)
-      redirect_to admin_user_path(@user), notice: "アドミンユーザー情報を更新しました"
+      redirect_to admin_user_path(@user), notice: "ユーザー#{@user.name}情報を更新しました"
+    else
+      redirect_to edit_admin_user_path(@user), notice: "管理者がいなくなるためユーザー#{@user.name}情報を更新できません"
     end
   end
 
   def destroy
  #   binding.pry
     @user = User.find(params[:id])
-    @user.destroy
-    redirect_to admin_users_path, notice: "アドミンユーザーを消去しました"
+    if @user.destroy
+      redirect_to admin_users_path, notice: "ユーザー#{@user.name}を消去しました"
+    else
+      redirect_to admin_users_path, notice:"管理者がいなくなるため#{@user.name}を消去できません"
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
 class User < ApplicationRecord
-  has_many :tasks, dependent: :destroy
-
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true,  length: { maximum: 255 }, uniqueness: true,
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
   validates :password, length: { minimum: 6 }
+  before_update :admin_cannot_update
+  before_destroy :admin_cannot_delete
+  has_many :tasks, dependent: :destroy
+
+  private
+
+  def admin_cannot_update
+    throw :abort if User.where(admin: true).count == 1 && self.admin_change == [true, false]
+  end
+
+  def admin_cannot_delete
+    throw :abort if User.where(admin: true).count == 1 && self.admin == true
+  end
 end
